45bc98a42ccdc5b7672a23451392be6551d19969
Daniel Rigberg <daniel.rigberg@gmail.com>
2018-12-27 19:50:46 -0500

    fix logout bug

--- ui/src/store/actions.js
+++ ui/src/store/actions.js
@@ -5,142 +5,155 @@ function getMessageFromError(err) {
   return err && err.response && err.response.data && err.response.data.message
 }
 
-export default {
-  async loadData({ commit }) {
-    const [contact, projects, banner] = await Promise.all([
-      axios.get('/api/contact').then(({ data }) => data),
-      axios.get('/api/projects').then(({ data }) => data),
-      axios.get('/api/banner').then(({ data }) => data)
-    ])
+async function loadData({ commit }) {
+  const [contact, projects, banner] = await Promise.all([
+    axios.get('/api/contact').then(({ data }) => data),
+    axios.get('/api/projects').then(({ data }) => data),
+    axios.get('/api/banner').then(({ data }) => data)
+  ])
 
-    commit('contact', contact)
-    commit('projects', projects)
-    commit('banner', banner)
-    commit('dataLoaded')
-  },
-  async resetPassword({ commit }, { username }) {
-    try {
-      const { data } = await axios
-        .post('/api/reset_credentials', {
-          username
-        })
+  commit('contact', contact)
+  commit('projects', projects)
+  commit('banner', banner)
+  commit('dataLoaded')
+}
+async function resetPassword({ commit }, { username }) {
+  try {
+    const { data } = await axios
+      .post('/api/reset_credentials', {
+        username
+      })
 
-      if (!data.success) {
-        commit('flashMessage', {
-          message: 'Invalid username/password combo, brah',
-          page: 'login'
-        })
-        return
-      }
-
+    if (!data.success) {
       commit('flashMessage', {
-        message: 'Credentials reset! You should get an email with your new password.',
+        message: 'Invalid username/password combo, brah',
         page: 'login'
       })
-    } catch (err) {
-      const message = getMessageFromError(err) || err.message
-      commit('flashMessage', {
-        message: `Error resetting password: ${message}`,
-        page: 'login'
-      })
+      return
     }
-  },
-  async login({ commit }, { username, password }) {
-    try {
-      const { data } = await axios
-        .post('/api/login', {
-          username,
-          password
-        })
 
-      if (!data.success) {
-        commit('flashMessage', {
-          message: 'Invalid username/password combo, brah',
-          page: 'login'
-        })
-        return
-      }
-
-      commit('loginSuccess')
-      commit('flashMessage', {
-        message: 'Welcome, site owner!',
-        page: '*'
-      })
-    } catch (err) {
-      const message = getMessageFromError(err) || err.message
-
-      commit('flashMessage', {
-        message: `Error logging in: ${message}`,
-        page: 'login'
-      })
-    }
-  },
-  async updateContact({ commit }, payload) {
-    try {
-      const { data } = await axios.post('/api/contact', payload)
-      commit('contact', data)
-      commit('flashMessage', {
-        message: 'Contact info updated!',
-        page: 'contact'
-      })
-    } catch (err) {
-      const message = getMessageFromError(err) || err.message
-
-      commit('flashMessage', {
-        message: `Error updating contact info: ${message}`,
-        page: 'contact'
-      })
-    }
-  },
-  async logout({ commit }) {
-    try {
-      await axios.get('/api/logout')
-      commit('logoutSuccess')
-
-      router.push('/')
-    } catch (err) {
-      const message = getMessageFromError(err) || err.message
-
-      commit('flashMessage', {
-        message: `Unable to logout! ${message}`,
-        page: 'banner'
-      })
-    }
-  },
-  async updateBanner({ commit }, payload) {
-    try {
-      const { data } = await axios.post('/api/banner', payload)
-      commit('banner', data)
-      commit('flashMessage', {
-        message: 'Banner info updated!',
-        page: 'banner'
-      })
-    } catch (err) {
-      const message = getMessageFromError(err) || err.message
-
-      commit('flashMessage', {
-        message: `Error updating banner info: ${message}`,
-        page: 'banner'
-      })
-    }
-  },
-  async updateCredentials({ commit }, payload) {
-    try {
-      await axios.post('/api/change_credentials', payload)
-
-      commit('flashMessage', {
-        message: 'Credentials updated!',
-        page: 'admin'
-      })
-
-      commit('logout')
-    } catch (err) {
-      const message = getMessageFromError(err) || err.message
-
-      commit('flashMessage', {
-        message: `Error updating credentials: ${message}`,
-        page: 'admin'
-      })
-    }
+    commit('flashMessage', {
+      message: 'Credentials reset! You should get an email with your new password.',
+      page: 'login'
+    })
+  } catch (err) {
+    const message = getMessageFromError(err) || err.message
+    commit('flashMessage', {
+      message: `Error resetting password: ${message}`,
+      page: 'login'
+    })
   }
+}
+
+async function login({ commit }, { username, password }) {
+  try {
+    const { data } = await axios
+      .post('/api/login', {
+        username,
+        password
+      })
+
+    if (!data.success) {
+      commit('flashMessage', {
+        message: 'Invalid username/password combo, brah',
+        page: 'login'
+      })
+      return
+    }
+
+    commit('loginSuccess')
+    commit('flashMessage', {
+      message: 'Welcome, site owner!',
+      page: '*'
+    })
+  } catch (err) {
+    const message = getMessageFromError(err) || err.message
+
+    commit('flashMessage', {
+      message: `Error logging in: ${message}`,
+      page: 'login'
+    })
+  }
+}
+
+async function updateContact({ commit }, payload) {
+  try {
+    const { data } = await axios.post('/api/contact', payload)
+    commit('contact', data)
+    commit('flashMessage', {
+      message: 'Contact info updated!',
+      page: 'contact'
+    })
+  } catch (err) {
+    const message = getMessageFromError(err) || err.message
+
+    commit('flashMessage', {
+      message: `Error updating contact info: ${message}`,
+      page: 'contact'
+    })
+  }
+}
+
+async function logout({ commit }) {
+  try {
+    await axios.get('/api/logout')
+    commit('logoutSuccess')
+
+    router.push('/')
+  } catch (err) {
+    const message = getMessageFromError(err) || err.message
+
+    commit('flashMessage', {
+      message: `Unable to logout! ${message}`,
+      page: 'banner'
+    })
+  }
+}
+
+async function updateBanner({ commit }, payload) {
+  try {
+    const { data } = await axios.post('/api/banner', payload)
+    commit('banner', data)
+    commit('flashMessage', {
+      message: 'Banner info updated!',
+      page: 'banner'
+    })
+  } catch (err) {
+    const message = getMessageFromError(err) || err.message
+
+    commit('flashMessage', {
+      message: `Error updating banner info: ${message}`,
+      page: 'banner'
+    })
+  }
+}
+
+async function updateCredentials({ commit }, payload) {
+  try {
+    await axios.post('/api/change_credentials', payload)
+
+    commit('flashMessage', {
+      message: 'Credentials updated!',
+      page: 'admin'
+    })
+
+    logout({ commit })
+  } catch (err) {
+    const message = getMessageFromError(err) || err.message
+
+    commit('flashMessage', {
+      message: `Error updating credentials: ${message}`,
+      page: 'admin'
+    })
+  }
+}
+
+export default {
+  loadData,
+  resetPassword,
+  login,
+  updateContact,
+  logout,
+  updateBanner,
+  updateCredentials
 }
